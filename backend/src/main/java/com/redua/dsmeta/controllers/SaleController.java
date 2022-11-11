3b88bf8ef98d24daaa0f308224f7ed0f1615f226
Joney Pereira <joney.pereira.ext@conductor.com.br>
2022-11-11 19:37:05 -0300

    Criado a consulta por data min e max

--- backend/src/main/java/com/redua/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/redua/dsmeta/controllers/SaleController.java
@@ -3,11 +3,12 @@ package com.redua.dsmeta.controllers;
 import com.redua.dsmeta.entities.Sale;
 import com.redua.dsmeta.services.SalesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,7 +18,9 @@ public class SaleController {
     private SalesService service;
 
     @GetMapping
-    public List<Sale> buscaVendas(){
-        return service.findSales();
+    public Page<Sale> buscaVendas(@RequestParam(value = "minDate", defaultValue = "") String minDate,
+                                  @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+                                  Pageable pageable){
+        return service.findSales(minDate, maxDate, pageable);
     }
 }
--- backend/src/main/java/com/redua/dsmeta/services/SalesService.java
+++ backend/src/main/java/com/redua/dsmeta/services/SalesService.java
@@ -3,8 +3,11 @@ package com.redua.dsmeta.services;
 import com.redua.dsmeta.entities.Sale;
 import com.redua.dsmeta.repositories.SalesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -13,7 +16,10 @@ public class SalesService {
     @Autowired
     private SalesRepository repository;
 
-    public List<Sale> findSales(){
-        return repository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable){
+        LocalDate min = LocalDate.parse(minDate);
+        LocalDate max = LocalDate.parse(maxDate);
+
+        return repository.findSales(min, max, pageable);
     }
 }
